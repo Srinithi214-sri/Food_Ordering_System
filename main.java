68b8c12ea1794de359f45ce95c391a7cac1cd485
Srinithi <premak602@gmail.com>
2026-09-25 10:36:30 -0700

    Modified main

--- main.java
+++ main.java
@@ -1,15 +1,20 @@
 import controller.CustomerController;
 import controller.RestaurantController;
 import repository.FoodRepository;
+import repository.RestaurantRepository;
 import view.CustomerView;
 import view.RestaurantView;
 public class main{
     public static void main(String[] args){
         FoodRepository foodrepo=new FoodRepository();
-        CustomerController customer=new CustomerController(foodrepo);
-        RestaurantController restaurant=new RestaurantController(foodrepo);
+        RestaurantRepository restaurantRepo = new RestaurantRepository();
+
+       CustomerController customer = new CustomerController(restaurantRepo, foodrepo);
+        RestaurantController restaurant = new RestaurantController(restaurantRepo, foodrepo);
+
         RestaurantView restaurantView = new RestaurantView();
         restaurantView.start(restaurant);
+
         CustomerView customerView = new CustomerView();
         customerView.start(customer);
     }
